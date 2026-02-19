2bcaff5d582f937161d22a04d0e120b3685585e2
1034pdacc-beep <1034.pdacc@gmail.com>
2026-02-19 19:30:36 +0530

    Create page.tsx

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import TicketsTable from "@/components/tickets-table"
+
+export default function DashboardPage() {
+  return (
+    <div className="p-6 space-y-6">
+      <TicketsTable />
+    </div>
+  )
+}
